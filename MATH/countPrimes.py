f4bf3c37b0e68beb4429508fda31d056fe91992c
Aundre Labrador <aundrejlabrador@student.hartnell.edu>
2020-09-07 21:38:47 -0700

    added Count Primes problem

---
+++ MATH/countPrimes.py
@@ -0,0 +1,23 @@
+'''
+
+Prompt:
+Count the number of prime numbers less than a non-negative number, n.
+
+'''
+
+# Determine if the input number is prime 
+def isPrime(n):
+  for current_number in range(2,n):
+    # if the input number is evenly divisible by the current number?
+    if n % current_number == 0:
+      return False
+  return True
+
+# Determine how many prime numbers are UNDER the input number
+def countPrimes(n):
+  count = 0
+  for i in range(2, n):
+    if isPrime(i) == True:
+      count += 1
+  return count
+
