c507848b0b5154f4a33edf6ed430211e491bf5c0
Aundre Labrador <aundrejlabrador@student.hartnell.edu>
2020-09-07 21:46:21 -0700

    added testcases and detailed instructions

--- MATH/countPrimes.py
+++ MATH/countPrimes.py
@@ -3,7 +3,17 @@
 Prompt:
 Count the number of prime numbers less than a non-negative number, n.
 
+Given 10, your countPrimes solution should return 4
+Given 20, your countPrimes solution should return 8
+
+1 does not count as a prime number.
+
 '''
+
+
+############
+# Solution #
+############
 
 # Determine if the input number is prime 
 def isPrime(n):
@@ -21,3 +31,19 @@ def countPrimes(n):
       count += 1
   return count
 
+
+###########
+# Testing #
+###########
+
+# Test 1 
+# Expected Result: 9
+print(countPrimes(25))
+
+# Test 2
+# Expected Result: 18
+print(countPrimes(64))
+
+# Test 3
+# Expected Result: 30
+print(countPrimes(125))
